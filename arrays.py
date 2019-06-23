da84b61a68a81c8f115345d66447f40f54cb9a2a
pradip148 <52080012+pradip148@users.noreply.github.com>
2019-06-23 03:49:24 -0700

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -1,9 +1,9 @@
 from array import *
-"""vals=array('i',[ 8,5,6,7,4 ])
+vals=array('i',[ 8,5,6,7,4 ])
 newarray=array(vals.typecode,(b for b in vals))
 print(newarray)
 print(id(vals))
-print(id(newarray))"""
+print(id(newarray))
 
 
 """ taking values from user"""
@@ -15,7 +15,7 @@ for i in range(n):
     arr.append(a)
 print(arr)
 
-""" printing the index 
+""" printing the index """
 
 n1=int(input("enter the value which is in array"))
 for i in range (n):
@@ -24,15 +24,14 @@ for i in range (n):
     else:
         print(" given value is not present in array ")
 
-
-  creat array with 5 values and delete value at index no. 2 , without using built-in function 
+"""creat array with 5 values and delete value at index no. 2 , without using built-in function""" 
 
 
 for i in range (n):
     if arr[2]!=arr[i]:
         print(arr[i]) 
 
-revese the array without using built-in function """
+"""revese the array without using built-in function """
 rearr=array('i',[])
 for i in range (1,n+1):
     x=(arr[n-i])
